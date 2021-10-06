6cb7d48a0b4a6b76a7627f38095c27f92cf48553
lucas <lucas.lins.lemos@hotmail.com>
2021-10-05 21:21:33 -0300

    upando

---
+++ Cadastros/F_clientes.vb
@@ -0,0 +1,13 @@
+﻿Public Class F_clientes
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+    End Sub
+End Class
