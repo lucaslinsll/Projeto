4fdbb69a6f8e57e099a7240844d328d6450dfb11
lucaslinsll <72295505+lucaslinsll@users.noreply.github.com>
2021-10-15 03:05:48 -0300

    Add files via upload

---
+++ Atualizado/Cadastros/Cadastro_Ferramentas.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Cadastro_Ferramentas
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Cadastro_Ferramentas))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtDescricao = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtBuscar = New System.Windows.Forms.TextBox()
+        Me.txtCodigo = New System.Windows.Forms.TextBox()
+        Me.dg = New System.Windows.Forms.DataGridView()
+        Me.btnSalvar = New System.Windows.Forms.Button()
+        Me.btnNovo = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.btnExcluir = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblRegistros = New System.Windows.Forms.Label()
+        CType(Me.dg, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 67)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ferramenta:"
+        '
+        'txtDescricao
+        '
+        Me.txtDescricao.Enabled = False
+        Me.txtDescricao.Location = New System.Drawing.Point(81, 62)
+        Me.txtDescricao.Name = "txtDescricao"
+        Me.txtDescricao.Size = New System.Drawing.Size(115, 20)
+        Me.txtDescricao.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(410, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Buscar:"
+        '
+        'txtBuscar
+        '
+        Me.txtBuscar.Location = New System.Drawing.Point(459, 36)
+        Me.txtBuscar.Name = "txtBuscar"
+        Me.txtBuscar.Size = New System.Drawing.Size(80, 20)
+        Me.txtBuscar.TabIndex = 3
+        '
+        'txtCodigo
+        '
+        Me.txtCodigo.Location = New System.Drawing.Point(81, 36)
+        Me.txtCodigo.Name = "txtCodigo"
+        Me.txtCodigo.Size = New System.Drawing.Size(44, 20)
+        Me.txtCodigo.TabIndex = 4
+        '
+        'dg
+        '
+        Me.dg.AllowUserToAddRows = False
+        Me.dg.AllowUserToDeleteRows = False
+        Me.dg.BackgroundColor = System.Drawing.Color.White
+        Me.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dg.Location = New System.Drawing.Point(15, 130)
+        Me.dg.Name = "dg"
+        Me.dg.ReadOnly = True
+        Me.dg.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dg.Size = New System.Drawing.Size(524, 150)
+        Me.dg.TabIndex = 5
+        '
+        'btnSalvar
+        '
+        Me.btnSalvar.BackColor = System.Drawing.Color.Transparent
+        Me.btnSalvar.BackgroundImage = CType(resources.GetObject("btnSalvar.BackgroundImage"), System.Drawing.Image)
+        Me.btnSalvar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnSalvar.Enabled = False
+        Me.btnSalvar.FlatAppearance.BorderSize = 0
+        Me.btnSalvar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Khaki
+        Me.btnSalvar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Khaki
+        Me.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSalvar.Location = New System.Drawing.Point(203, 326)
+        Me.btnSalvar.Name = "btnSalvar"
+        Me.btnSalvar.Size = New System.Drawing.Size(60, 60)
+        Me.btnSalvar.TabIndex = 6
+        Me.btnSalvar.UseVisualStyleBackColor = False
+        '
+        'btnNovo
+        '
+        Me.btnNovo.BackColor = System.Drawing.Color.Transparent
+        Me.btnNovo.BackgroundImage = CType(resources.GetObject("btnNovo.BackgroundImage"), System.Drawing.Image)
+        Me.btnNovo.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnNovo.FlatAppearance.BorderSize = 0
+        Me.btnNovo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Khaki
+        Me.btnNovo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Khaki
+        Me.btnNovo.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnNovo.Location = New System.Drawing.Point(121, 326)
+        Me.btnNovo.Name = "btnNovo"
+        Me.btnNovo.Size = New System.Drawing.Size(60, 60)
+        Me.btnNovo.TabIndex = 7
+        Me.btnNovo.UseVisualStyleBackColor = False
+        '
+        'btnEditar
+        '
+        Me.btnEditar.BackColor = System.Drawing.Color.Transparent
+        Me.btnEditar.BackgroundImage = CType(resources.GetObject("btnEditar.BackgroundImage"), System.Drawing.Image)
+        Me.btnEditar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnEditar.Enabled = False
+        Me.btnEditar.FlatAppearance.BorderSize = 0
+        Me.btnEditar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Khaki
+        Me.btnEditar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Khaki
+        Me.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnEditar.Location = New System.Drawing.Point(283, 326)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(60, 60)
+        Me.btnEditar.TabIndex = 8
+        Me.btnEditar.UseVisualStyleBackColor = False
+        '
+        'btnExcluir
+        '
+        Me.btnExcluir.BackColor = System.Drawing.Color.Transparent
+        Me.btnExcluir.BackgroundImage = CType(resources.GetObject("btnExcluir.BackgroundImage"), System.Drawing.Image)
+        Me.btnExcluir.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnExcluir.Enabled = False
+        Me.btnExcluir.FlatAppearance.BorderSize = 0
+        Me.btnExcluir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Khaki
+        Me.btnExcluir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Khaki
+        Me.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExcluir.Location = New System.Drawing.Point(368, 326)
+        Me.btnExcluir.Name = "btnExcluir"
+        Me.btnExcluir.Size = New System.Drawing.Size(60, 60)
+        Me.btnExcluir.TabIndex = 9
+        Me.btnExcluir.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(410, 300)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(54, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Registros:"
+        '
+        'lblRegistros
+        '
+        Me.lblRegistros.AutoSize = True
+        Me.lblRegistros.Location = New System.Drawing.Point(470, 300)
+        Me.lblRegistros.Name = "lblRegistros"
+        Me.lblRegistros.Size = New System.Drawing.Size(10, 13)
+        Me.lblRegistros.TabIndex = 11
+        Me.lblRegistros.Text = "-"
+        '
+        'Cadastro_Ferramentas
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkKhaki
+        Me.ClientSize = New System.Drawing.Size(558, 410)
+        Me.Controls.Add(Me.lblRegistros)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnExcluir)
+        Me.Controls.Add(Me.btnEditar)
+        Me.Controls.Add(Me.btnNovo)
+        Me.Controls.Add(Me.btnSalvar)
+        Me.Controls.Add(Me.dg)
+        Me.Controls.Add(Me.txtCodigo)
+        Me.Controls.Add(Me.txtBuscar)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtDescricao)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Cadastro_Ferramentas"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Cadastro Ferramentas"
+        CType(Me.dg, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtDescricao As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtBuscar As TextBox
+    Friend WithEvents txtCodigo As TextBox
+    Friend WithEvents dg As DataGridView
+    Friend WithEvents btnSalvar As Button
+    Friend WithEvents btnNovo As Button
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents btnExcluir As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblRegistros As Label
+End Class
---
+++ Atualizado/Login.vb
@@ -0,0 +1,44 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class F_login
+    Private Sub BTN_login_Click(sender As Object, e As EventArgs) Handles BTN_login.Click
+        Try
+            abrir()
+            Dim cmdUser As MySqlCommand
+            Dim reader As MySqlDataReader
+            Dim sql2 As String
+            sql2 = "select * from usuarios where login_usuario = '" & txtLogin.Text & "' and
+                   senha = '" & txtSenha.Text & "'"
+            cmdUser = New MySqlCommand(sql2, con)
+            reader = cmdUser.ExecuteReader
+
+            If reader.Read = True Then
+                MsgBox("Bem vindo(ª), " + txtLogin.Text)
+
+                Me.Hide()
+                F_principal.Show()
+                fechar()
+
+
+            Else
+                fechar()
+                abrir()
+                MsgBox("Usuário ou senha incorretos! ")
+
+            End If
+        Catch ex As Exception
+            MsgBox("Erro ao logar" + ex.Message)
+        End Try
+
+
+
+    End Sub
+
+    Private Sub F_login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub linkCadUsuario_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles linkCadUsuario.LinkClicked
+        Cadastro_Usuario.Show()
+    End Sub
+End Class
